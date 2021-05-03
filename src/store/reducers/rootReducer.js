c72fb5658749fd46990b181db58ecf6d7d87ddce
prathapsomesh <prathap966@gmail.com>
2021-05-03 15:08:13 +0530

    firebase integrated

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux'
 import PatientReducer from './patientRequestReducer'
+import { firebaseReducer } from 'react-redux-firebase'
+import authReducer from './authReducer'
 
 const rootReducer = combineReducers({
-    patient : PatientReducer,
+    auth: authReducer,
+    patient: PatientReducer,
+    firebase: firebaseReducer
 });
 
 export default rootReducer;
