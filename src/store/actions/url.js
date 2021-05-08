4ade9b038502e45b1fba7b30bc9d26c268652358
prathapsomesh <prathap966@gmail.com>
2021-05-08 19:07:42 +0530

    url changes

--- src/store/actions/url.js
+++ src/store/actions/url.js
@@ -1,2 +1,2 @@
-// export const getUrl = 'http://covidassistindia-env-1.eba-r3fivnem.us-east-1.elasticbeanstalk.com/';
-export const getUrl = 'http:///127.0.0.1:8080/';
+export const getUrl = 'http://covidassistindia-env-2.eba-m2yx7azr.us-east-1.elasticbeanstalk.com/';
+// export const getUrl = 'http:///127.0.0.1:8080/';
