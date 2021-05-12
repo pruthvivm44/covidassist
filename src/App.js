dd53c5c9c135fcdee5bb2472d77ee5517a915467
prathapsomesh <prathap966@gmail.com>
2021-05-12 18:33:56 +0530

    footer changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Navibar from './components/navbar/navibar';
 import {BrowserRouter,Route} from 'react-router-dom'
 import Banner from './components/banner/banner';
 import Volunteer from './components/volunteer/volunteer'
-import Footer from './components/footer';
 function App() {
   return (
     <BrowserRouter>
@@ -10,7 +9,6 @@ function App() {
         <Navibar />
         <Route exact path={process.env.PUBLIC_URL +'/'} component={Banner}/>
         <Route exact path={process.env.PUBLIC_URL +'/volunteer'} component={Volunteer}/>
-        <Footer />
       </div>
     </BrowserRouter>
   );
