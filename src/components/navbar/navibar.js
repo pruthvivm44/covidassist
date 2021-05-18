cb755b8921674ea2608bd20f3d3d79d9434278cc
prathapsomesh <prathap966@gmail.com>
2021-05-18 12:29:34 +0530

    added scroll and banner image with bug fixes

--- src/components/navbar/navibar.js
+++ src/components/navbar/navibar.js
@@ -6,26 +6,40 @@ import { Link, NavLink } from 'react-router-dom';
 import { connect } from 'react-redux'
 import { signOut } from '../../store/actions/authAction'
 
-const Navibar = (props) => {
+class Navibar extends React.Component {
+    scrollToMyRef = () =>{
+        // Get the navbar
+        var navbar = document.getElementById("patient");
+        var offsetTop =  navbar.offsetTop;
+                window.scrollTo({
+                    top:offsetTop, 
+                    behavior:'smooth'})  ;
+    } 
+    // run this method to execute scrolling. 
+    render(){
         return(
             <Navbar collapseOnSelect bg="white" expand="md" variant="light" className="p-4">
                 <Navbar.Brand className="p-0" ><NavLink to={'/'}><img src={logo} alt="Logo" width="210px;" /></NavLink></Navbar.Brand>
                 <Navbar.Toggle color="#000"  aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mr-auto"></Nav>
-                    <Nav.Link className="black hoverred" to={'/'} as={Link}>HOME</Nav.Link>
-                    <Nav.Link className="black hoverred" to={'/leads'} as={Link}>LEADS</Nav.Link>
-                    {props.auth.uid ?
+                    <Nav.Link className="black hoverred" onClick={this.scrollToMyRef}>
+                        PATIENT</Nav.Link>
+                    {this.props.auth.uid ?
                     <NavDropdown title="ACCOUNT" id="basic-nav-dropdown" className="black hoverred">
                         <NavDropdown.Item href="# " to={'/volunteer'} as={Link}>Dashboard</NavDropdown.Item>
-                        <NavDropdown.Item href="# " onClick={()=>{props.signOut()}}>Sign out</NavDropdown.Item>
+                        <NavDropdown.Item href="# " onClick={()=>{this.props.signOut()}}>Sign out</NavDropdown.Item>
                     </NavDropdown>
                     :
                     <Nav.Link className="black hoverred" to={'/volunteer'} as={Link}>VOLUNTEER</Nav.Link>
                     }
+                    <Nav.Link className="black hoverred" to={'/leads'} as={Link}>LEADS</Nav.Link>
+                   
                 </Navbar.Collapse>
             </Navbar>
         )
+    }
+  
 
   }
   const mapStateToProps = (state) => {
